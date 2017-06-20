c30655ed94e6d11187ca085060aa785675d962f1
Student <Student@SDEH131-08C4DL>
2017-06-19 22:54:04 -0500

    Almost done. Need to check test data. Double check sorting.

--- HillClimb/HillClimb.py
+++ HillClimb/HillClimb.py
@@ -16,10 +16,11 @@
 
 
 import queue
+import copy
 from collections import deque
 
 class Tree:
-    def __init__(self, cargo ,left=None, right=None):
+    def __init__(self, cargo, level = None ,left=None, right=None):
         self.cargo = cargo
         self.left  = left
         self.right = right
@@ -30,41 +31,79 @@ class Tree:
 
 def make_tree():
     
-    t = Tree("S", Tree("A", Tree("B", Tree("C"), Tree("E", Tree("D"), Tree("F", Tree("G")))),
-    Tree("D", Tree("E", Tree("B", Tree("C")), Tree("F", Tree("G"))))), Tree("D", Tree("A", Tree("B", Tree("C"),
-    Tree("E", Tree("F", Tree("G"))))), Tree("E", Tree("B", Tree("A"), Tree("C")), Tree("F", Tree("G")))))
+    t = Tree("S", 0 , Tree("A", 1, Tree("B",2, Tree("C", 3), Tree("E", 3, Tree("D", 4), Tree("F", 4, Tree("G",5)))),
+    Tree("D", 2, Tree("E",3, Tree("B",4, Tree("C",5)), Tree("F",4, Tree("G",5))))), Tree("D",1, Tree("A",2, Tree("B",3, Tree("C",4),
+    Tree("E",4, Tree("F",5, Tree("G",6))))), Tree("E",2, Tree("B",3, Tree("A",4), Tree("C",4)), Tree("F",3, Tree("G",4)))))
+
+#    t = Tree("S", 0 , Tree("A", 1, Tree("B",2, Tree("C", 3), Tree("E", 3, Tree("D"), Tree("F", Tree("G")))),
+ #   Tree("D", Tree("E", Tree("B", Tree("C")), Tree("F", Tree("G"))))), Tree("D",1, Tree("A", Tree("B", Tree("C"),
+  #  Tree("E", Tree("F", Tree("G"))))), Tree("E", Tree("B", Tree("A"), Tree("C")), Tree("F", Tree("G")))))
+
+
     return t
 
 
-def sort():
-    print("lol")
+def sort(q):
+    
+    
+    q2 = queue.PriorityQueue
+    q2.queue = copy.deepcopy(q.queue)
+
+    i = q.qsize()
+    
+    while i > 0:
+        temp = q.get()
+        
+        print("Cargo",temp.cargo)
+        #q2.put(temp.cargo, temp)
+        i-=1
+
+    q.queue = copy.deepcopy(q2.queue)
+    
+
 
 def hill_climb(q):
     print ("Hill climb")
 
-    t = make_tree    
+    t = make_tree()    
+    
+    goalNode = "G"
 
-    
-    
+    #Enter root into a queue
+    q.put(t)
+ 
     #While there are elements in the queue
-        #print ()
+    while q.qsize() != 0:
+        print ("Hill climb")
+        t = q.get()
+               
+
+        #Determine if first element is goal node
+   
+        print("Current node:",t.cargo)
+
         
+        #2a if the first element is the goal node, break
+        if t.cargo == goalNode:
+            print("Goal found! Success!")
+            break;
+    
+        #2b else, 
+        else:
+            #Remove first element from the queue(Done at the beginning) and sort the first elements children. Enter at the FRONT of the queue.
+            
+            if t.left != None:
+                q.put(t.left)
+            if t.right != None:
+                q.put(t.right)
+
+            #Sort!
+            sort(q)
+            
 
 def main():
 
     q = queue.LifoQueue()
-    #q.put("S") # Create first root object
-
-    #q = deque("S")
-    #q.appendleft('A')
-
-    
-    
-    print("Hello World")
     hill_climb(q)
-    #Hill_climb
-    
-    
-    
 
 main()
